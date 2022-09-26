6291f2e038c818a2e0949d3b97db76dfdb7dfc71
FaridAnggoro-dev <you@example.com>
2022-09-26 21:44:46 +0700

    Merubah Informasi Welcome React Native

--- App.js
+++ App.js
@@ -76,8 +76,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            Hello world
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
